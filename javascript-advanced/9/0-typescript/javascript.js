646f629027589c7f512aae4bef1f60a1c39d9be3
Jakub Wąsik <32049761+Eghizio@users.noreply.github.com>
2024-12-09 19:12:46 +0100

    JavaScript Advanced Class 9 - before

---
+++ javascript-advanced/9/0-typescript/javascript.js
@@ -0,0 +1,11 @@
+console.log("Hello from JavaScript 👋");
+
+console.log(1 + 1);
+console.log("1" + "1");
+
+function add(a, b) {
+  return a + b;
+}
+
+console.log(add(1, 1));
+console.log(add("1", "1"));
