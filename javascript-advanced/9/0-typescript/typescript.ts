652118f7540d6007b8644b7dea7232aa18433b1a
Jakub Wąsik <32049761+Eghizio@users.noreply.github.com>
2024-12-09 20:57:18 +0100

    JavaScript Advanced Class 9 (#16)
    * JavaScript Advanced Class 9 - before
    
    * JavaScript Advanced Class 9 - after

---
+++ javascript-advanced/9/0-typescript/typescript.ts
@@ -0,0 +1,22 @@
+console.log("Hello from TypeScript 👋");
+
+console.log(1 + 1);
+console.log("1" + "1");
+
+// function sum(a, b) {
+//     return a + b;
+// }
+
+// function sum(a: number, b: number): number {
+//   return a + b;
+// }
+
+/* Type inference for return value. */
+function sum(a: number, b: number) {
+    return a + b;
+}
+
+console.log(sum(1, 1));
+// console.log(sum("1", "1")); /* Error: Type 'string' is not assignable to type 'number'. */
+
+console.log(sum(42, 2));
