d3c4b4ec1cc58ad9f5afac7d1e30b8971587732c
Jakub Wąsik <32049761+Eghizio@users.noreply.github.com>
2024-12-16 23:14:07 +0100

    Rainbow links

--- javascript-advanced/11/src/12-uml-diagrams.ts
+++ javascript-advanced/11/src/12-uml-diagrams.ts
@@ -1,6 +1,34 @@
+import { Colors } from "./colors";
+
 export {};
 
 /* UML - Unified Modeling Language */
 
 // https://app.diagrams.net
 // https://refactoring.guru/design-patterns/catalog
+
+const randomColor = (): Colors => {
+  const colors = Object.values(Colors).slice(1); // Exclude Black.
+  return colors[Math.floor(Math.random() * colors.length)];
+};
+
+const printAllColors = (text: string): void => {
+  const colors = Object.values(Colors);
+
+  colors.forEach((color) => {
+    console.log(color, text);
+  });
+
+  console.log("\n");
+};
+
+const umls = "https://app.diagrams.net";
+const designPatterns = "https://refactoring.guru/design-patterns/catalog";
+
+console.log("Ctrl+Click link 🔗", "\n");
+
+console.log(randomColor(), umls);
+printAllColors(umls);
+
+console.log(randomColor(), designPatterns);
+printAllColors(designPatterns);
