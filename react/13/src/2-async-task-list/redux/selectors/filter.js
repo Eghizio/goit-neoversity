4276e60c600364e38cb04de50d3bfa4a6fab9c6b
Jakub Wąsik <32049761+Eghizio@users.noreply.github.com>
2025-03-10 20:46:23 +0100

    React Class 13 - after

--- react/13/src/2-async-task-list/redux/selectors/filter.js
+++
@@ -1,2 +0,0 @@
-/* We can define selectors in a Slice as well. */
-export const getFilterStatus = (state) => state.filters.status;
--- react/13/src/2-async-task-list/redux/selectors/tasks.js
+++
@@ -1,2 +0,0 @@
-/* We can define selectors in a Slice as well. */
-export const getTasks = (state) => state.tasks;
